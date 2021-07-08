76d6be96f9b6c4d03e7ae52a80be1a1d49647b33
lucianagg <lucianagolvet28@gmail.com>
2021-07-08 11:40:19 +0200

    1140

--- app/src/main/java/com/example/projetcommun/WSUtils.java
+++ app/src/main/java/com/example/projetcommun/WSUtils.java
@@ -1,5 +1,30 @@
 package com.example.projetcommun;
 
-public class WSUtils {
+import com.google.android.gms.maps.model.LatLng;
 
+public class WSUtils {
+    public static PointBean loadWeather(double latitude, double longitude) throws Exception {
+        String url =  "&lat=" + latitude + "&lon=" + longitude;
+        //Requete
+        String json = OkhttpUtils.sendGetOkHttpRequest(url);
+        //Parser le JSON
+        System.out.println("Json = " + json);
+        PointBean pointBean = new Gson().fromJson(json, PointBean.class);
+        return pointBean;
+    }
+
+    public static LatLng getISSPosition() throws Exception {
+        //Requete
+        String json = OkhttpUtils.sendGetOkHttpRequest();
+        //Parser le JSON
+        System.out.println("Json = " + json);
+        ISSWSResponseBean isswsResponseBean = new Gson().fromJson(json, ISSWSResponseBean.class);
+        if (isswsResponseBean.getIss_position() == null
+                || isswsResponseBean.getIss_position().latitude == 0
+                || isswsResponseBean.getIss_position().longitude == 0) {
+            throw new Exception("Pas de position");
+        }
+
+        return isswsResponseBean.getIss_position();
+    }
 }
